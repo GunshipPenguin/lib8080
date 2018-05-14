e7dd0972d57182cce271301e6062893fce45ab6e
Rhys Rustad-Elliott <me@rhysre.net>
2018-05-13 22:28:13 -0400

    Test all cases for cmc and cma instructions

--- test/unit/cpu/carry_bit.c
+++ test/unit/cpu/carry_bit.c
@@ -14,7 +14,7 @@ AFTER_EACH() {
   teardown_cpu_test_env(cpu);
 }
 
-TEST_CASE(stc) {
+TEST_CASE(stc_carry_0) {
   write_byte(cpu, 0, 0x37); // STC
   set_flag(cpu, FLAG_C, 0);
 
@@ -24,12 +24,32 @@ TEST_CASE(stc) {
   ASSERT_EQUAL(cpu->PC, 1);
 }
 
-TEST_CASE(cmc) {
+TEST_CASE(stc_carry_1) {
+  write_byte(cpu, 0, 0x37); // STC
+  set_flag(cpu, FLAG_C, 1);
+
+  step_cpu(cpu);
+
+  ASSERT_TRUE(get_flag(cpu, FLAG_C));
+  ASSERT_EQUAL(cpu->PC, 1);
+}
+
+TEST_CASE(cmc_carry_1) {
   write_byte(cpu, 0, 0x3F); // CMC
   set_flag(cpu, FLAG_C, 1);
 
   step_cpu(cpu);
 
   ASSERT_FALSE(get_flag(cpu, FLAG_C));
+  ASSERT_EQUAL(cpu->PC, 1);
+}
+
+TEST_CASE(cmc_carry_0) {
+  write_byte(cpu, 0, 0x3F); // CMC
+  set_flag(cpu, FLAG_C, 0);
+
+  step_cpu(cpu);
+
+  ASSERT_TRUE(get_flag(cpu, FLAG_C));
   ASSERT_EQUAL(cpu->PC, 1);
 }
